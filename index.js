cccedfe147b30b949498311932f0a1226f557d4a
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-13 16:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,8 @@ function placeOrder(cardNumber) {
 
   }
 
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`  ;
-
+  var g =  `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`  ;
+  cart = [];
+  return g;
 
 }
