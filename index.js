5fe30c2dc6953f1212b3d19518156b1160e6e804
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-13 16:50:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,6 +70,6 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
 
   }
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  document.write(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   cart = [];
 }
