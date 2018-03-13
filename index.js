1c9539ecde75d0279503fd3f41e2239c0c8b75ef
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-13 16:46:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,12 +67,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber === undefined) {
-    return `Sorry, we don't have a credit card on file for you.`;
+    console.log(`Sorry, we don't have a credit card on file for you.`);
+    return false;
   }
-  let total = 0;
-  while(cart.length > 0) {
-    total += cart[0].itemPrice;
-    cart.shift();
-  }
-  return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  cart = [];
 }
